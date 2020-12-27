2745a17d44f906308567c461871f6a7bbc805ed4
esauerbo <esauerbo@calpoly.edu>
2020-12-27 12:41:58 -0800

    part 4

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,8 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const Recipe = require('./models/recipe-schema');
 
-/*app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
-*/
-
-app.get('/api/recipe', (req, res) => {
-    res.status(200)
-    res.send('List of recipes requested')
-  })
-
-app.get('/api/recipe/:name', (req, res) => {
-res.status(200)
-let name = req.params.name
-res.send('Instructions for '+ name +' requested')
-})
-
-app.post("/api/rating", (req, res) =>{
-    res.status(200)
-    let rating = req.body.rating
-    let id = req.body.id
-    res.send(`Rating of '+ ${rating} for recipe ${id}`)
-  })
 
 mongoose.connect("mongodb+srv://dbUser:dbUserPassword@cluster0.cy6ha.mongodb.net/<dbname>?retryWrites=true&w=majority", {
   useNewUrlParser: true,
@@ -36,4 +15,36 @@ app.use(express.static(__dirname + "/../public"))
 
 app.use('/files', express.static('public'))
 
-app.listen(5000)
+app.get('/api/recipe', async (req, res) => {
+    console.log('recipes page')
+    res.status(200)
+    let recipes = await Recipe.find();
+    res.json(recipes);
+  })
+
+app.get('/api/recipe/:name', async (req, res) => {
+  res.status(200)
+  let name = req.params.name;
+  console.log(name + " recipe")
+  let recipe = await Recipe.find({
+    "title" : name
+  });
+  res.json(recipe);
+})
+
+app.post("/api/rating", async (req, res) =>{
+    res.status(200)
+    let rating = req.body.rating
+    let id = req.body.id
+    const response = await Recipe.update(
+      {"title": id},
+      {$push: {ratings: rating}},
+  );
+    res.send(`Rating of '+ ${rating} for recipe ${id}`)
+  })
+
+
+
+app.listen(5000, function(){
+  console.log("Started on port 5000")
+})
